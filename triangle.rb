bc3e409ad62fa6cbe17c32db1c53040d9b4fdc63
Gwendolyn Schmidt <gwenix@gmail.com>
2018-07-03 16:42:22 -0400

    wrote my first ruby code\!

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a!=b && b!=c && c!=a 
+    return :scalene
+  elsif a!=b || b!=c || c!=a
+    return :isosceles
+  else
+    return :equilateral
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
